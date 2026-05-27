cade1f757add1d691ca35e027df731790f67e677
h-ryushin <115770963+h-ryushin@users.noreply.github.com>
2026-05-27 11:18:11 +0900

    gitignoreの修正

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,15 +3,17 @@ import { getFirestore } from "firebase/firestore";
 
 // 先ほどの画面でコピーした config をここに貼り付け！
 const firebaseConfig = {
-  apiKey: "AIzaSyDlCM4_4Iw3oqsK_dsIQCbk__N3NyTylF8",
-  authDomain: "kintai-app-98723.firebaseapp.com",
-  projectId: "kintai-app-98723",
-  storageBucket: "kintai-app-98723.firebasestorage.app",
-  messagingSenderId: "376665760461",
-  appId: "1:376665760461:web:1614c853c88ea7e38a7f84",
-  measurementId: "G-17WD5RMMY2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
+
